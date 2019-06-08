a54a2d2f8d4bc6d7d4859d02e29932c92ad60d4a
Matthew <mfarmer5102@gmail.com>
2019-06-08 19:16:59 -0400

    Reformatted code to be more object oriented

--- logic.js
+++ logic.js
@@ -1,98 +1,110 @@
-// Example queryURL for Giphy API
-let apiKey = 'totZ3Gjqp9mJMwb9urodXV1OIB0cppGk';
-
+//################## GLOBAL VARIABLES ##############################################################################################################################
 
 //List of Options
 let topics = ['Croissants', 'Noodles', 'Sushi', 'Pizza', 'Tacos', 'Soup', 'Salad', 'Pasta']
 let grabbedObjects = []
-let favorites = []
 
-//Render Buttons
-renderButtons()
 
-//Event Listeners
-$(document).on("click", ".keywordButtons", function () {
 
-    //Search Parameters
-    let searchTerms = $(this).val();
-    let rating = 'r';
-    let limit = '10';
+//################## OBJECTS #######################################################################################################################################
 
-    //Compiled Query
-    var queryURL = `http://api.giphy.com/v1/gifs/search?q=${searchTerms}&rating=${rating}&limit=${limit}&api_key=${apiKey}`;
+/************************** Settings **************************/
+let settings = {
+    apiKey: 'totZ3Gjqp9mJMwb9urodXV1OIB0cppGk',
+    rating: 'r',
+    limit: 10
+}
 
-    //Upon calling
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-    }).then(function (response) {
-        for (i = 0; i < response.data.length; i++) {
-            var obj = {};
-            obj.id = response.data[i].id
-            obj.imgAnimated = response.data[i].images.original.url
-            obj.imgStill = response.data[i].images.original_still.url
-            obj.rating = response.data[i].rating
-            obj.isFavorite = false;
-            grabbedObjects.push(obj)
-        };
-        console.log(grabbedObjects)
-        renderGrabbedObjects()
-    });
+/************************** Query **************************/
+let query = {
+    searchByTitle: function (arg) {
+        $.ajax({
+            url: `http://api.giphy.com/v1/gifs/search?q=${arg}&rating=${settings.rating}&limit=${settings.limit}&api_key=${settings.apiKey}`,
+            method: "GET",
+        }).then(function (response) {
+            console.log(response)
 
-});
+            for (i = 0; i < response.data.length; i++) {
+                var obj = {};
+                obj.id = response.data[i].id
+                obj.imgAnimated = response.data[i].images.original.url
+                obj.imgStill = response.data[i].images.original_still.url
+                obj.rating = response.data[i].rating
+                obj.isFavorite = false;
+                grabbedObjects.push(obj)
+            };
+            console.log(grabbedObjects)
+            importedGifs.render()
 
-//Animate or deanimate a GIF upon clicking
-$(document).on("click", ".returnedGIF", function () {
-    let ID = $(this).attr("id");
-    console.log(ID)
-    let animatedURL = $(this).attr("data-animatedurl");
-    let stillURL = $(this).attr("data-stillurl");
-    let isAnimated = $(this).attr("data-isanimated");
-    if (isAnimated === 'false') {
-        $(`#${ID}`).attr('src', animatedURL)
-        $(`#${ID}`).attr('data-isanimated', 'true')
-    } else {
-        $(`#${ID}`).attr('src', stillURL)
-        $(`#${ID}`).attr('data-isanimated', 'false')
-    }
-    addToFavorites(ID);
-    console.log('favorited items: ' + favorites)
-});
-
-function addNewSearchWord() {
-    let wordToBeAdded = $('#keywordInput').val();
-    topics.unshift(wordToBeAdded)
-    renderButtons()
-    //$("#buttonContainer").prepend(`<button class='btn btn-success mb-3 ml-1 mr-1' value='${wordToBeAdded}'>${wordToBeAdded}</button>`)
-};
-
-function renderButtons() {
-    $("#buttonContainer").empty()
-    for (i = 0; i < topics.length; i++) {
-        $("#buttonContainer").append(`<button class='btn btn-success mb-3 ml-1 mr-1 keywordButtons' value='${topics[i]}'>${topics[i]}</button>`)
+        });
     }
 };
 
-function renderGrabbedObjects() {
-    for (i = 0; i < grabbedObjects.length; i++) {
-        let tempID = grabbedObjects[i].id
-        $("#gifContainer").prepend(`
-            <div id="${tempID}-div" class='m-2 p-2 shadow' style="background: white">
-                <img height='150px' width='150px' id="${grabbedObjects[i].id}" class="returnedGIF" data-isanimated="false" data-stillurl="${grabbedObjects[i].imgStill}" data-animatedurl="${grabbedObjects[i].imgAnimated}" src=${grabbedObjects[i].imgStill}>
-                <div class="card-footer">
-                    <button><i class="far fa-heart"></i></button>
-                    <br/>
-                    <small>Rating: ${grabbedObjects[i].rating.toUpperCase()}</small>
-                </div>
-            </div>
-        `)
-    };
-};
-
-function addToFavorites(passedId) {
-    for (i=0; i<grabbedObjects.length; i++) {
-        if (grabbedObjects[i].id === passedId) {
-            favorites.push(grabbedObjects[i].id)
+/************************** Buttons **************************/
+let buttons = {
+    add: function () {
+        let wordToBeAdded = $('#keywordInput').val();
+        topics.unshift(wordToBeAdded)
+        this.render()
+    },
+    render: function () {
+        $("#buttonContainer").empty()
+        for (i = 0; i < topics.length; i++) {
+            $("#buttonContainer").append(`<button class='btn btn-success mb-3 ml-1 mr-1 keywordButtons' value='${topics[i]}'>${topics[i]}</button>`)
         }
     }
-}
+};
+
+/************************** Imported GIFs **************************/
+let importedGifs = {
+    toggleAnimation: function (arg) {
+        let tag = `#${arg}`
+        let animatedURL = $(tag).attr("data-animatedurl");
+        let stillURL = $(tag).attr("data-stillurl");
+        let isAnimated = $(tag).attr("data-isanimated");
+        if (isAnimated === 'false') {
+            $(tag).attr('src', animatedURL)
+            $(tag).attr('data-isanimated', 'true')
+        } else {
+            $(tag).attr('src', stillURL)
+            $(tag).attr('data-isanimated', 'false')
+        }
+    },
+    render: function () {
+        for (i = 0; i < grabbedObjects.length; i++) {
+            let tempID = grabbedObjects[i].id
+            $("#gifContainer").prepend(`
+                <div id="${tempID}-div" class='m-2 p-2 shadow' style="background: white">
+                    <img height='150px' width='150px' id="${grabbedObjects[i].id}" class="returnedGIF" data-isanimated="false" data-stillurl="${grabbedObjects[i].imgStill}" data-animatedurl="${grabbedObjects[i].imgAnimated}" src=${grabbedObjects[i].imgStill}>
+                    <div class="card-footer">
+                        <button><i class="far fa-heart"></i></button>
+                        <br/>
+                        <small>Rating: ${grabbedObjects[i].rating.toUpperCase()}</small>
+                    </div>
+                </div>
+            `)
+        }
+    }
+};
+
+
+
+//################## EVENT LISTENERS #################################################################################################################################
+
+//Show GIFs on button click
+$(document).on("click", ".keywordButtons", function () {
+    let searchTerms = $(this).val();
+    query.searchByTitle(searchTerms)
+});
+
+//Toggle animation
+$(document).on("click", ".returnedGIF", function () {
+    let ID = $(this).attr("id");
+    importedGifs.toggleAnimation(ID)
+});
+
+
+
+//################## RUN PROGRAM #####################################################################################################################################
+
+buttons.render()

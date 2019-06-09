4945464ceb2a0908bfecdcd16f411768ca830855
Matthew <mfarmer5102@gmail.com>
2019-06-09 16:16:50 -0400

    isolated card draw function

--- logic.js
+++ logic.js
@@ -1,10 +1,10 @@
-//################## GLOBAL VARIABLES ##############################################################################################################################
-
-//List of Options
-let topics = ['Croissants', 'Noodles', 'Sushi', 'Pizza', 'Tacos', 'Soup', 'Salad', 'Pasta']
-let grabbedObjects = []
-
 //################## OBJECTS #######################################################################################################################################
+
+/************************** Global **************************/
+let global = {
+    topics: ['Croissants', 'Noodles', 'Sushi', 'Pizza', 'Tacos', 'Soup', 'Salad', 'Pasta'],
+    grabbedObjects: []
+}
 
 /************************** Settings **************************/
 let settings = {
@@ -21,13 +21,13 @@ let query = {
             method: "GET",
         }).then(function (response) {
             for (i = 0; i < response.data.length; i++) {
-                grabbedObjects.push(response.data[i])
+                global.grabbedObjects.push(response.data[i])
             };
-            console.log(grabbedObjects)
+            console.log(global.grabbedObjects)
             importedGifs.render()
 
             //Clear out the array(s) for the next query
-            grabbedObjects = [];
+            global.grabbedObjects = [];
         });
     },
     searchByID: function (arg) {
@@ -52,21 +52,27 @@ let query = {
 let buttons = {
     add: function () {
         let wordToBeAdded = $('#keywordInput').val();
-        topics.unshift(wordToBeAdded)
+        global.topics.unshift(wordToBeAdded)
         this.render()
     },
     render: function () {
         $("#buttonContainer").empty()
-        for (i = 0; i < topics.length; i++) {
-            $("#buttonContainer").append(`<button class='btn btn-warning mb-3 ml-1 mr-1 keywordButtons' value='${topics[i]}'>${topics[i]}</button>`)
+        for (i = 0; i < global.topics.length; i++) {
+            $("#buttonContainer").append(`<button class='btn btn-warning mb-3 ml-1 mr-1 keywordButtons' value='${global.topics[i]}'>${global.topics[i]}</button>`)
         }
     }
 };
 
 /************************** Imported GIFs **************************/
 let card = {
-    draw: function () {
-
+    draw: function (receivingContainer, object) {
+        $(receivingContainer).prepend(`
+        <div id="${object.id}-div" class='m-1' style="position: relative">
+            <img height='150px' width='150px' id="${object.id}" class="returnedGIF" data-isanimated="false" data-isfavorited="false" data-stillurl="${object.images.original_still.url}" data-animatedurl="${object.images.original.url}" src=${object.images.original_still.url}>
+            <div class='loveButton' style="position: absolute; right: 5px; top: 5px;"><i class="far fa-heart"></i></div>
+            <div class='bg-light text-center' style="position: absolute; left: 5px; bottom: 5px; width: 50px; opacity: 0.7">${object.rating.toUpperCase()}</div>
+        </div>
+        `)
     }
 }
 
@@ -86,15 +92,9 @@ let importedGifs = {
         }
     },
     render: function () {
-        for (i = 0; i < grabbedObjects.length; i++) {
-            let tempID = grabbedObjects[i].id
-            $("#gifContainer").prepend(`
-                <div id="${tempID}-div" class='m-1' style="position: relative">
-                    <img height='150px' width='150px' id="${grabbedObjects[i].id}" class="returnedGIF" data-isanimated="false" data-isfavorited="false" data-stillurl="${grabbedObjects[i].images.original_still.url}" data-animatedurl="${grabbedObjects[i].images.original.url}" src=${grabbedObjects[i].images.original_still.url}>
-                    <div class='loveButton' style="position: absolute; right: 5px; top: 5px;"><i class="far fa-heart"></i></div>
-                    <div class='bg-light text-center' style="position: absolute; left: 5px; bottom: 5px; width: 50px; opacity: 0.7">${grabbedObjects[i].rating.toUpperCase()}</div>
-                </div>
-            `)
+        for (i = 0; i < global.grabbedObjects.length; i++) {
+            let temp = global.grabbedObjects[i]
+            card.draw('#gifContainer', temp)
         }
     }
 };
@@ -120,15 +120,15 @@ let favorites = {
             let tempID = favorites.collectionObj[i].id
             $("#favoritesContainer").prepend(`
                 <div id="${tempID}-div" class='m-1'>
-                    <img height='150px' width='150px' id="${favorites.collectionObj[i].id}-favorited" data-isanimated="false" data-stillurl="${favorites.collectionObj[i].images.original_still.url}" data-animatedurl="${favorites.collectionObj[i].images.original.url}" src=${favorites.collectionObj[i].images.original.url}>
+                    <img height='150px' width='150px' id="${favorites.collectionObj[i].id}-favorited" class='returnedGIF' data-isanimated="false" data-stillurl="${favorites.collectionObj[i].images.original_still.url}" data-animatedurl="${favorites.collectionObj[i].images.original.url}" src=${favorites.collectionObj[i].images.original_still.url}>
                 </div>
             `)
         }
     },
-    writeToStorage: function() {
+    writeToStorage: function () {
         localStorage.setItem('favorites', `${this.package}`);
     },
-    readFromStorage: function() {
+    readFromStorage: function () {
         var favs = localStorage.getItem('favorites');
         console.log('favs are ' + favs)
         this.package.push(favs)
@@ -155,12 +155,12 @@ $(document).on("click", ".returnedGIF", function () {
 
 //Add to favorites
 $(document).on("click", ".loveButton", function () {
-    let favoriteTest = $(this).siblings( ".returnedGIF" ).attr("data-isfavorited")
-    if ( favoriteTest === 'false'){
+    let favoriteTest = $(this).siblings(".returnedGIF").attr("data-isfavorited")
+    if (favoriteTest === 'false') {
         favoriteTest = 'true'
         $(this).html('<i class="fas fa-heart"></i>')
-        let id = $(this).siblings( ".returnedGIF" ).attr("id");
-        let associatedImage = $(this).siblings( ".returnedGIF" )
+        let id = $(this).siblings(".returnedGIF").attr("id");
+        let associatedImage = $(this).siblings(".returnedGIF")
         favorites.add(associatedImage, id)
     } else {
         alert('not false')
